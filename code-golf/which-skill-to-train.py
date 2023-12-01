757f980474bd2ccf43b7453f4f3d0897ef71f7b2
Riahiamirreza <riahiamirreza79@gmail.com>
2023-12-01 15:00:41 +0330

    indentifying code

--- code-golf/which-skill-to-train.py
+++ code-golf/which-skill-to-train.py
@@ -1,7 +1,29 @@
 # https://codegolf.stackexchange.com/questions/267099/which-skill-to-train
 
 
-x = (lambda skills: max((((lambda skills: (f := lambda numbers: numbers[0] if len(numbers) == 1 else numbers[0] * f(numbers[1:]))(list(pow(j, i) for i, j in enumerate(sorted(skills), start=2))))(j[0:i]+[j[i]+1]+j[i+1:]), i) for i, j in enumerate([skills]*len(skills))), key=lambda i:i[0])[1])
+x = (
+    lambda skills:
+        max(
+            (
+                (
+                    (
+                        lambda skills: (
+                            f := lambda numbers: numbers[0] if len(numbers) == 1 else numbers[0] * f(numbers[1:])
+                        )(list(pow(j, i) for i, j in enumerate(sorted(skills), start=2)))
+                    )(j[0:i]+[j[i]+1]+j[i+1:]), i
+                ) for i, j in enumerate([skills]*len(skills))
+            ),
+            key=lambda i:i[0]
+        )[1]
+)
+
+
+assert x([3, 2, 4, 1, 2])==1
+assert x([7, 19, 12, 20, 14])==3
+assert x([13, 12, 19, 9, 20])==0
+assert x([13, 18, 12, 12, 14])==4
+assert x([18, 19, 18, 16, 13])==1
+assert x([14, 14, 19, 17, 11])==2
 
 # compact version
 e=enumerate
